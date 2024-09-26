650872c4f4bc88f258770380e0d0a75c5452f9d6
Daniel Felipe <daneelfx@gmail.com>
2024-09-26 17:10:01 -0500

    update settings

--- dragon/settings.py
+++ dragon/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&6vzpn&d$cah^$mv^&#ozk7-#o@4q7jv2*r8k2jp19z_&)^fnx'
+# SECRET_KEY = 'django-insecure-&6vzpn&d$cah^$mv^&#ozk7-#o@4q7jv2*r8k2jp19z_&)^fnx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
